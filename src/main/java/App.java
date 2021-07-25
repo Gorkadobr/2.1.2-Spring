633e8b696820fd30941a857fe6458f68af76cf17
gorkadobr <gorkadobrrr@yandex.ru>
2021-07-25 18:01:52 +0300

    Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        boolean hello = bean == bean1;
+        System.out.println(bean.getMessage() + " == " + bean1.getMessage() + " = " + hello);
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        boolean catt = bean2 == bean3;
+        System.out.println(bean2.getMessage() + " == " + bean3.getMessage() + " = " + catt);
+
     }
 }
